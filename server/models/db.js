c671088b57b9fded2f1515f9d5c2357433fc45da
cryptic-pr03 <priyanshurmn1702@gmail.com>
2022-04-08 12:42:50 +0530

    db modified

---
+++ server/models/db.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const studentSchema = mongoose.Schema({
+    fname: String,
+    mname: String,
+    lname: String,
+    contact: Number,
+    resumeLink : String,
+    email: {
+        type: String,
+        required: true
+    },
+    roll: { // will serve as primary key
+        type: Number,
+        required: true
+    },
+    selectedProff: {
+        type: String,
+        default: null
+    },
+    currentCPI: {
+        type: Number,
+        required : true
+    },
+    proffOrder: {
+        type: [String] // stroresId
+    }
+});
+
+
+const proffSchema = mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    fName: String,
+    mName : String,
+    lName : String,
+    total: {
+        type : Number,
+        default: 10,
+    },
+    selectedStudents: {
+        type: [Number]
+    }
+
+});
+
+
+// const preferenceSchema = mongoose.Schema({
+//     roll: {
+//         type: Number,
+//         required: true
+//     },
+//     order: {
+//         type: [String] // stroresId
+//     }
+// });
+
+
+const db = { 
+    Student : mongoose.model("Student", studentSchema),
+    Proff : mongoose.model("Proff", proffSchema),
+    // Preference: mongoose.model("Preference", preferenceSchema),
+} 
+
+module.exports = db;
