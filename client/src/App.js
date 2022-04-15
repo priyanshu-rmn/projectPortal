57ffabdaa137f9b8fc91cd401e55821f643372cb
Vanshika-kejriwal <kejriwalvanshika@gmail.com>
2022-04-15 17:55:48 +0530

    login page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
-
-const App = () => {
-    return (
-        <>
-         <NavBar/>  
-        </>
-        )
-};
+import Login_page from './Components/Login_page';
+function App() {
+  return (
+    <>
+     <NavBar/>
+      <Login_page/>
+    </>
+  );
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+// import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 
-ReactDOM.render(
-    <App />, document.getElementById('root'),
-);
+// ReactDOM.render(
+//     <App />, document.getElementById('root'),
+// );
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App/>)
