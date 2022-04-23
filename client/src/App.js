aaa92fad4e741c742f5d8947844c4ec6d1e7f228
cryptic-pr03 <priyanshurmn1702@gmail.com>
2022-04-23 15:49:06 +0530

    initialised client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import NavBar from './Components/NavBar';
-import LoginPage from './Components/LoginPage';
-import RegForm from './Components/RegForm';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-     <NavBar/>
-     <Routes>
-         <Route exact path='/' element= {<LoginPage/>}/>
-         < Route path='/regform' element= {<RegForm/>}/>
-     </Routes>
-     
-   
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
